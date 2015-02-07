5c5ee5ba0cddbbcbe9c89dfb291ae24571061fd5
Jackeagle <jackeagle102@gmail.com>
2015-02-07 11:31:00 -0500

    Glade Vendor Setup
    Change-Id: Ief94aa8ebee2e8243e603db872e4dd0dc4b731eb
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: kirmaniarslan <arslankirmani9600@gmail.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,10 +1,10 @@
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+# Inherit common Glade stuff
+$(call inherit-product, vendor/glade/config/common.mk)
 
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
+# Include Glade audio files
+include vendor/glade/config/glade_audio.mk
 
-# Optional CM packages
+# Optional Glade packages
 PRODUCT_PACKAGES += \
     Galaxy4 \
     HoloSpiralWallpaper \
@@ -18,7 +18,7 @@ PRODUCT_PACKAGES += \
     SoundRecorder \
     PhotoPhase
 
-# Extra tools in CM
+# Extra tools in Glade
 PRODUCT_PACKAGES += \
     vim \
     zip \
